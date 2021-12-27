12db86e9b2df922c61fd4cfd5241ac303ec72a46
kuksag <georgiy.kuksa@gmail.com>
2021-12-27 20:13:45 +0300

    Simplified brute force

--- main.py
+++ main.py
@@ -8,29 +8,28 @@ LOGGER_FILE = 'execution.log'
 CONFIG_FILE = 'config.yaml'
 RESOURCE_FOLDER = 'resource'
 RESULT_FOLDER = 'result'
-created = 0
 
 logging.basicConfig(filename=LOGGER_FILE, level=logging.DEBUG)
 
 
-def brute_force_gen(objects, count=-1, accumulator=None, name='0'):
-    global created
-    if created == count:
-        return
-
+def brute_force_gen(objects, to_create, accumulator=None, name='0'):
+    """
+    Returns number of emojis that are to be created
+    """
     if not objects:
-        created += 1
         accumulator.save(f'{RESULT_FOLDER}/{name}.png')
-        logging.info(f'Generated {name}, {count - created} remaining')
-        return
+        logging.info(f'Generated {name}, {to_create - 1} remaining')
+        return to_create - 1
 
     for i, obj in enumerate(os.listdir(f'{RESOURCE_FOLDER}/{objects[0]}')):
         this_obj = Image.open(f'{RESOURCE_FOLDER}/{objects[0]}/{obj}')
         new_acc = copy.copy(accumulator) or this_obj
-        brute_force_gen(objects=objects[1:],
-                        count=count,
-                        accumulator=Image.alpha_composite(new_acc, this_obj),
-                        name=f'{name}-{i}')
+        if to_create != 0:
+            to_create = brute_force_gen(objects=objects[1:],
+                                        to_create=to_create,
+                                        accumulator=Image.alpha_composite(new_acc, this_obj),
+                                        name=f'{name}-{i}')
+    return to_create
 
 
 def run():
@@ -50,7 +49,7 @@ def run():
         config = yaml.safe_load(_config)
         logging.info(f"Using this config:\n{config}")
         brute_force_gen(objects=config['objects'],
-                        count=config['count'])
+                        to_create=config['count'])
 
 
 if __name__ == '__main__':
